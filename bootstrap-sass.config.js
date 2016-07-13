fe75b1f2a774aabcda4e3bef0a9259034e355f59
Katarinich <katarina.chirich@gmail.com>
2016-07-13 23:04:09 +0300

    Change webpack config, so now there is bootstrap styles

---
+++ bootstrap-sass.config.js
@@ -0,0 +1,58 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  verbose: false, // print out your custom files used
+  debug: false, // print out the full generated scss file
+  styleLoader: ExtractTextPlugin.extract("style-loader", "css-loader!sass"),
+  scripts: {
+    // add every bootstrap script you need
+    'transition': true,
+    'dropdown': true
+  },
+  styles: {
+    // add every bootstrap style you need
+    "mixins": true,
+
+    "normalize": true,
+    "print": true,
+    "glyphicons": true,
+
+    "scaffolding": true,
+    "type": true,
+    "code": true,
+    "grid": true,
+    "tables": true,
+    "forms": true,
+    "buttons": true,
+
+    "component-animations": true,
+    "dropdowns": true,
+    "button-groups": true,
+    "input-groups": true,
+    "navs": true,
+    "navbar": true,
+    "breadcrumbs": true,
+    "pagination": true,
+    "pager": true,
+    "labels": true,
+    "badges": true,
+    "jumbotron": true,
+    "thumbnails": true,
+    "alerts": true,
+    "progress-bars": true,
+    "media": true,
+    "list-group": true,
+    "panels": true,
+    "wells": true,
+    "responsive-embed": true,
+    "close": true,
+
+    "modals": true,
+    "tooltip": true,
+    "popovers": true,
+    "carousel": true,
+
+    "utilities": true,
+    "responsive-utilities": true
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,60 @@
 var path = require('path')
 var webpack = require('webpack')
 
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var buildDir = 'build';
+var targetDir = 'static/' + buildDir;
+var publicPath = '/' + buildDir + '/';
+
 var config = {
   devtool: 'cheap-source-map',
   entry: [
     'webpack-hot-middleware/client?path=/__webpack_hmr&reload=true',
+    "bootstrap-sass!./bootstrap-sass.config.js",
     './app/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, targetDir),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: publicPath
   },
   plugins: [
     new webpack.IgnorePlugin(/vertx/), // Isomorphic fetch workaround
     new webpack.IgnorePlugin(/iconv.*/), // Isomorphic fetch workaround
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+      }),
+
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('app.css', {
+        allChunks: true
+      })
   ],
+  progress: true,
+  //
+  stats: {
+    colors: true,
+    reasons: true
+  },
   module: {
     loaders: [
-      {
-        test: /\.json$/,
-        loader: 'json',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.js$/,
-        loaders: ['babel'],
-        exclude: /node_modules/, 
-        include: __dirname
-      },
-      {
-        test: /\.js$/,
-        loader: 'eslint',
-        include: [
-          path.join(__dirname, 'src')
-        ]
-      }
+      { test: /\.json$/, loader: 'json', exclude: /node_modules/ },
+      { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/, include: __dirname },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css') },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!sass') },
+      { test: /\.(png|jpg)$/, loader: 'url?limit=25000' },
+      { test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+      { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+      { test: /\.js$/, loader: 'eslint', include: [ path.join(__dirname, 'src') ] }
     ]
   },
   eslint: {
